64dbff79f0ecef696b241674ae1b9ce415855dc6
Johan Ronnas <johanronnas@gmail.com>
2020-04-18 17:07:27 +0200

    Added cannon and started with UI

---
+++ Assets/CastleHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CastleHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    Slider healthSlider;
+
+    int health;
+    int maxHealth;
+    void Start()
+    {
+        healthSlider.maxValue = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TakeDamage()
+    {
+        health -= 1;
+        healthSlider.value = health;
+    }
+
+    public void Heal()
+    {
+
+    }
+}
